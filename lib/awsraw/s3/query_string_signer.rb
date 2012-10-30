00dc9ecc8ef59e69f7d6fb3bcc7d47a92a1b3bb6
David Goodlad <david@goodlad.net>
2012-10-30 21:16:30 +1100

    Make QueryStringSigner a subclass of Signer

--- lib/awsraw/s3/query_string_signer.rb
+++ lib/awsraw/s3/query_string_signer.rb
@@ -1,8 +1,5 @@
-require 'digest/sha1'
-require 'openssl'
-require 'uri'
+require 'awsraw/s3/signer'
 require 'cgi'
-require 'base64'
 
 module AWSRaw
   module S3
@@ -10,13 +7,7 @@ module AWSRaw
     # Generates the signed query string for an authenticated GET request to S3
     #
     # See http://docs.amazonwebservices.com/AmazonS3/latest/dev/RESTAuthentication.html#RESTAuthenticationQueryStringAuth
-    class QueryStringSigner
-
-      def initialize(access_key_id, secret_access_key)
-        @access_key_id     = access_key_id
-        @secret_access_key = secret_access_key
-      end
-
+    class QueryStringSigner < Signer
       def query_string(bucket, object, expires, headers = {})
         query_string_hash(bucket, object, expires, headers).map { |k,v|
           "#{k}=#{v}"
@@ -25,10 +16,7 @@ module AWSRaw
 
       def query_string_hash(bucket, object, expires, headers = {})
         string_to_sign = string_to_sign(bucket, object, expires, headers)
-
-        digest    = OpenSSL::Digest::Digest.new("sha1")
-        sha       = OpenSSL::HMAC.digest(digest, @secret_access_key, string_to_sign)
-        signature = Base64.encode64(sha).strip
+        signature = encoded_signature(string_to_sign)
 
         {
           "AWSAccessKeyId" => @access_key_id,
@@ -49,16 +37,6 @@ module AWSRaw
       end
 
     private
-
-      def canonicalized_amz_headers(headers)
-        header_names = headers.keys.
-          select  {|name| name =~ /^x-amz-/i }.
-          sort_by {|name| name.downcase }
-
-        header_names.map do |name|
-          "#{name.downcase}:#{headers[name]}"
-        end
-      end
 
       # Assumes that bucket and object are already URI-encoded!
       def canonicalized_resource(bucket, object)
