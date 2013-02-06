52698b0d3da1091974a19852770986b17473c24b
Shanon McQuay <shanonmcquay@gmail.com>
2013-02-06 16:52:06 +1100

    Improve the formatting of the request spec.

--- spec/s3/request_spec.rb
+++ spec/s3/request_spec.rb
@@ -10,7 +10,7 @@ describe AWSRaw::S3::Request do
   let(:signer)            { AWSRaw::S3::Signer.new(access_key, secret_access_key) }
   let(:params)            { Hash.new }
 
-  subject { AWSRaw::S3::Request.new(params, signer) }
+  subject(:s3_request) { AWSRaw::S3::Request.new(params, signer) }
 
   before do
     params[:bucket] = bucket
@@ -19,34 +19,33 @@ describe AWSRaw::S3::Request do
   describe "#host" do
 
     it "defaults to the standard host" do
-      subject.host.should == "s3.amazonaws.com"
+      s3_request.host.should == "s3.amazonaws.com"
     end
 
     it "uses the region specific host when a region is supplied" do
       params[:region] = 'ap-southeast-2'
 
-      subject.host.should == "s3-ap-southeast-2.amazonaws.com"
+      s3_request.host.should == "s3-ap-southeast-2.amazonaws.com"
     end
   end
 
   describe "#path" do
 
     it "includes the bucket name" do
-      subject.path.should == "/#{bucket}"
+      s3_request.path.should == "/#{bucket}"
     end
 
     context "when a key has been supplied" do
 
       before do
-        params[:key]    = key
+        params[:key] = key
       end
 
       it "includes the bucket name and key" do
-        subject.path.should == "/#{bucket}/#{key}"
+        s3_request.path.should == "/#{bucket}/#{key}"
       end
     end
 
   end
 
 end
-
