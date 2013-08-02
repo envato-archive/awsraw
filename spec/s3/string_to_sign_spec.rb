0daa0338c81caaff8105e039c42db6f088533ece
Pete Yandell <pete@notahat.com>
2013-08-02 15:23:09 +1000

    Better specs for generating S3 string to sign.

--- spec/s3/string_to_sign_spec.rb
+++ spec/s3/string_to_sign_spec.rb
@@ -1,72 +1,117 @@
+require 'awsraw/s3/string_to_sign'
 
+describe AWSRaw::S3::StringToSign do
 
-module StringToSign
-  def self.string_to_sign(request_info = {})
-    [
-      request_info[:method],
-      request_info[:content_md5]  || "",
-      request_info[:content_type] || "",
-      request_info[:date],
-      canonicalized_amz_headers(request_info[:amz_headers] || {}),
-      canonicalized_resource(request_info[:bucket], request_info[:key], request_info[:subresource])
-    ].flatten.join("\n")
+  # Examples are pulled from the AWS docs: http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
+
+  it "Example Object GET" do
+    request = {
+      :method  => "GET",
+      :bucket  => "johnsmith",
+      :key     => "photos/puppy.jpg",
+      :date    => "Tue, 27 Mar 2007 19:36:42 +0000"
+    }
+
+    expect(described_class.string_to_sign(request)).to eq(
+      "GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg"
+    )
   end
 
-private
+  it "Example Object PUT" do
+    request = {
+      :method       => "PUT",
+      :bucket       => "johnsmith",
+      :key          => "photos/puppy.jpg",
+      :content_type => "image/jpeg",
+      :date         => "Tue, 27 Mar 2007 21:15:45 +0000"
+    }
 
-  def self.canonicalized_amz_headers(headers)
-    header_names = headers.keys.
-      select  {|name| name =~ /^x-amz-/i }.
-      sort_by {|name| name.downcase }
-
-    header_names.map do |name|
-      "#{name.downcase}:#{headers[name]}"
-    end
+    expect(described_class.string_to_sign(request)).to eq(
+      "PUT\n\nimage/jpeg\nTue, 27 Mar 2007 21:15:45 +0000\n/johnsmith/photos/puppy.jpg"
+    )
   end
 
-  def self.canonicalized_resource(bucket, key, subresource = nil)
-    "/#{bucket}/#{key}".tap do |resource|
-      resource << "?#{subresource}" if subresource
-    end
+  it "Example List" do
+    request = {
+      :method => "GET",
+      :bucket => "johnsmith",
+      :date   => "Tue, 27 Mar 2007 19:42:41 +0000"
+    }
+
+    expect(described_class.string_to_sign(request)).to eq(
+      "GET\n\n\nTue, 27 Mar 2007 19:42:41 +0000\n/johnsmith/"
+    )
   end
-end
 
+  it "Example Fetch" do
+    request = {
+      :method      => "GET",
+      :bucket      => "johnsmith",
+      :subresource => "acl",
+      :date        => "Tue, 27 Mar 2007 19:44:46 +0000"
+    }
 
-describe StringToSign do
+    expect(described_class.string_to_sign(request)).to eq(
+      "GET\n\n\nTue, 27 Mar 2007 19:44:46 +0000\n/johnsmith/?acl"
+    )
+  end
 
-  context "examples from Amazon docs" do
-    it "generates the right string to sign for a get request" do
-      request = {
-        :method  => "GET",
-        :host    => "s3.amazonaws.com",
-        :bucket  => "johnsmith",
-        :key     => "photos/puppy.jpg",
-        :date    => "Tue, 27 Mar 2007 19:36:42 +0000"
+  it "Example Delete" do
+    request = {
+      :method => "DELETE",
+      :bucket => "johnsmith",
+      :key    => "photos/puppy.jpg",
+      :date   => "Tue, 27 Mar 2007 21:20:26 +0000"
+    }
+
+    expect(described_class.string_to_sign(request)).to eq(
+      "DELETE\n\n\nTue, 27 Mar 2007 21:20:26 +0000\n/johnsmith/photos/puppy.jpg"
+    )
+  end
+
+  it "Example Upload" do
+    request = {
+      :method       => "PUT",
+      :bucket       => "static.johnsmith.net",
+      :key          => "db-backup.dat.gz",
+      :date         => "Tue, 27 Mar 2007 21:06:08 +0000",
+      :content_md5  => "4gJE4saaMU4BqNR0kLY+lw==",
+      :content_type => "application/x-download",
+      :amz_headers => {
+        "x-amz-acl"                    => "public-read",
+        "X-Amz-Meta-ReviewedBy"        => "joe@johnsmith.net,jane@johnsmith.net",
+        "X-Amz-Meta-FileChecksum"      => "0x02661779",
+        "X-Amz-Meta-ChecksumAlgorithm" => "crc32",
       }
+    }
 
-      expect(described_class.string_to_sign(request)).to eq("GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg")
-    end
+    expect(described_class.string_to_sign(request)).to eq(
+      "PUT\n4gJE4saaMU4BqNR0kLY+lw==\napplication/x-download\nTue, 27 Mar 2007 21:06:08 +0000\nx-amz-acl:public-read\nx-amz-meta-checksumalgorithm:crc32\nx-amz-meta-filechecksum:0x02661779\nx-amz-meta-reviewedby:joe@johnsmith.net,jane@johnsmith.net\n/static.johnsmith.net/db-backup.dat.gz"
+    )
+  end
 
-    it "generates the right string to sign for an upload" do
-      request = {
-        :method       => "PUT",
-        :bucket       => "static.johnsmith.net",
-        :key          => "db-backup.dat.gz",
-        :date         => "Tue, 27 Mar 2007 21:06:08 +0000",
-        :content_md5  => "4gJE4saaMU4BqNR0kLY+lw==",
-        :content_type => "application/x-download",
-        :amz_headers => {
-          "x-amz-acl"                    => "public-read",
-          "X-Amz-Meta-ReviewedBy"        => "joe@johnsmith.net,jane@johnsmith.net",
-          "X-Amz-Meta-FileChecksum"      => "0x02661779",
-          "X-Amz-Meta-ChecksumAlgorithm" => "crc32",
-        }
-      }
+  it "Example List All My Buckets" do
+    request = {
+      :method => "GET",
+      :date   => "Wed, 28 Mar 2007 01:29:59 +0000",
+    }
 
-      expect(described_class.string_to_sign(request)).to eq(
-        "PUT\n4gJE4saaMU4BqNR0kLY+lw==\napplication/x-download\nTue, 27 Mar 2007 21:06:08 +0000\nx-amz-acl:public-read\nx-amz-meta-checksumalgorithm:crc32\nx-amz-meta-filechecksum:0x02661779\nx-amz-meta-reviewedby:joe@johnsmith.net,jane@johnsmith.net\n/static.johnsmith.net/db-backup.dat.gz"
-      )
-    end
+    expect(described_class.string_to_sign(request)).to eq(
+      "GET\n\n\nWed, 28 Mar 2007 01:29:59 +0000\n/"
+    )
+  end
+
+  it "Example Unicode Keys" do
+    request = {
+      :method => "GET",
+      :bucket => "dictionary",
+      :key    => "fran%C3%A7ais/pr%c3%a9f%c3%a8re",
+      :date   => "Wed, 28 Mar 2007 01:49:49 +0000"
+    }
+
+    expect(described_class.string_to_sign(request)).to eq(
+      "GET\n\n\nWed, 28 Mar 2007 01:49:49 +0000\n/dictionary/fran%C3%A7ais/pr%c3%a9f%c3%a8re"
+    )
   end
 
 end
