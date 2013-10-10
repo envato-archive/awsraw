e15f63bbcc27c44dc6f0be1c89b058d43e7c4b67
Pete Yandell <pete@notahat.com>
2013-10-10 11:21:45 +1100

    Add missing requires to top level.

--- lib/awsraw.rb
+++ lib/awsraw.rb
@@ -1,6 +1,8 @@
+require "awsraw/error"
 require "awsraw/version"
 require "awsraw/s3/canonicalized_resource"
 require "awsraw/s3/client"
+require "awsraw/s3/content_md5_header"
 require "awsraw/s3/faraday_middleware"
 require "awsraw/s3/query_string_signer"
 require "awsraw/s3/signature"
